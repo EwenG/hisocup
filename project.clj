49cdefe2278b2dcb8a102cc86a99ef9fc307d50e
egr <egr@zengularity.com>
2015-09-26 21:57:47 +0200

    Requires clojure 1.7 (because of reader conditionals)

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
 (defproject hisocup "1.0.5"
   :description "A slightly modified version of hiccup which can be used to build isomorphic applications based on hiccup and reagent."
   :url "https://github.com/EwenG/hisocup"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [reagent "0.5.1"]]
-  :profiles
-  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [reagent "0.5.1"]])
